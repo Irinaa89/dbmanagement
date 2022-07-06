72b04f4fc48e66082c28899b92e43a9b7987b4d6
Irinaa89 <108801921+Irinaa89@users.noreply.github.com>
2022-07-06 14:19:00 +0300

    Initial configuration

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module app.dbmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens app.dbmanagement to javafx.fxml;
+    exports app.dbmanagement;
+}
