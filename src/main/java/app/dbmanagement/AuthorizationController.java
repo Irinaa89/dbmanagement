a2dd4c69c0fc2cdd002a49304dd4894a9a583627
Irinaa89 <108801921+Irinaa89@users.noreply.github.com>
2022-07-06 17:52:04 +0300

    creating windows

--- src/main/java/app/dbmanagement/AuthorizationController.java
+++ src/main/java/app/dbmanagement/AuthorizationController.java
@@ -1,14 +1,42 @@
 package app.dbmanagement;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class AuthorizationController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button loginInButton;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button signup;
+
+    @FXML
+    void initialize() {
+//        assert loginInButton != null : "fx:id=\"loginInButton\" was not injected: check your FXML file 'authorization.fxml'.";
+//        assert login_field != null : "fx:id=\"login_field\" was not injected: check your FXML file 'authorization.fxml'.";
+//        assert password_field != null : "fx:id=\"password_field\" was not injected: check your FXML file 'authorization.fxml'.";
+//        assert signup != null : "fx:id=\"signup\" was not injected: check your FXML file 'authorization.fxml'.";
+
+        loginInButton.setOnAction(event ->{
+            System.out.println("Войти");
+        });
     }
+
 }
